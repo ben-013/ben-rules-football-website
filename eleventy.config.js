c12c89fddf47de030b2dd9a3e45078f354023cc9
Ben McDonald <benmcdonald5542@outlook.com>
2026-07-27 20:57:52 +0100

    Eleventy project set up

---
+++ eleventy.config.js
@@ -0,0 +1,19 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  eleventyConfig.addFilter("dateDisplay", (date) =>
+    new Intl.DateTimeFormat("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+    }).format(date)
+  );
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
